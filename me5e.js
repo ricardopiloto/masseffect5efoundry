cdb35fbb38e33c5ff6a602cea72e205e493b87b7
ricardopiloto <ac.ricardosobral@gmail.com>
2022-06-11 22:44:14 -0300

    Bunch of changes, still can´

--- me5e.js
+++ me5e.js
@@ -1,12 +1,11 @@
-import { me5e } from "./module/config";
-import ME5EItemSheet from "./module/sheets/ME5EItemSheet";
+import { me5e } from "./module/config.js";
+import ME5EItemSheet from "./module/sheets/ME5EItemSheet.js";
 
-Hooks.once("init", function (){
+Hooks.once("init", function(){
     console.log("ME5e | Initializing Mass Effect 5e System");
 
     CONFIG.me5e = me5e;
-    
+
     Items.unregisterSheet("core", ItemSheet);
     Items.registerSheet("me5e", ME5EItemSheet, {makeDefault: true});
-}
-)
+});
--- module/sheets/ME5EItemSheet.js
+++ module/sheets/ME5EItemSheet.js
@@ -1,10 +1,11 @@
 export default class ME5EItemSheet extends ItemSheet {
     get template(){
-        return `systems/masseffect5efoundry/templates/sheets/${this.item.data.type}--sheet.html`;
+        return `systems/me5e/templates/sheets/${this.item.data.type}-sheet.html`;
     }
 
     getData(){
         const data = super.getData();
         data.config = CONFIG.me5e;
+        return data;
     }
 }
